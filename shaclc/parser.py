c66868d254281828bbea829093da6ca4a53993d0
Edmond Chuc <edmond@kurrawong.ai>
2024-01-13 02:20:03 +1000

    Format

--- shaclc/parser.py
+++ shaclc/parser.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 from lark import Lark
 
-grammar_path = Path(__file__).parent / 'grammar.lark'
+grammar_path = Path(__file__).parent / "grammar.lark"
 
 with open(grammar_path, "r", encoding="utf-8") as file:
     grammar = file.read()
